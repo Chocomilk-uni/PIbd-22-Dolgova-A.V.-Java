16e53b918275500d2b88199341e754cae8a85332
Chocomilk <kawa.no.kiri@gmail.com>
2020-11-04 17:18:49 +0400

    Labwork1 is ready.

---
+++ LabWork/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BusFrame busFrame = new BusFrame();
+        BusPanel busPanel = new BusPanel();
+        busFrame.addBusPanel(busPanel);
+    }
+}
